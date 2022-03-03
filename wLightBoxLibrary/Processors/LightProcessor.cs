7f72753997ed528e5e1065dca908f78242bed93f
szymale <83646262+szymale@users.noreply.github.com>
2022-03-03 19:54:32 +0100

    Created LightProcessor

---
+++ wLightBoxLibrary/Processors/LightProcessor.cs
@@ -0,0 +1,27 @@
+﻿namespace wLightBoxLibrary
+{
+    public class LightProcessor
+    {
+
+        // this class could be used to change both color and/or effect - depending on input in Rgbw lightSet
+        public static async Task<Rgbw> ChangeLight(string connectingMethod, Rgbw lightSet)
+        {
+            string url = $"{ connectingMethod }/api/rgbw/set";
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync<Rgbw>(url, lightSet))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    Rgbw light = await response.Content.ReadAsAsync<Rgbw>();
+
+                    return light;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(response.ReasonPhrase);
+                }
+            }
+
+        }
+    }
+}
